137b6107ba5cb8695ded50a899d2204041059ed0
Rana E. Fathin <ranaekakurniafathin@gmail.com>
2023-05-13 11:45:29 +0700

    router udah tapi masih bingung nge link ke halaman yang lain

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
-import { useState } from 'react'
-import { Navbar } from './components/Navbar'
-import { Main_content_wrap } from './components/Main_content_wrap'
-import { Footer } from './components/Footer'
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Home } from './Pages/Home'
+import About from './Pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="kurni turu Moment">
-        <Navbar></Navbar>
-
-        <div className="nama">
-            <h2>SAKU.co</h2>
-            <h3>Sistem Pemesanan Kantin UPNVJ</h3>
-        </div>
-
-        <Main_content_wrap></Main_content_wrap>
-
-        <Swiper
-          spaceBetween={10}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          >
-          <SwiperSlide><img className="sushi rounded-lg" src="./img/sushi.jpg" alt="" /></SwiperSlide>
-          <SwiperSlide><img className="burger rounded-lg" src="./img/burgir.jpg" alt="" /></SwiperSlide>
-          <SwiperSlide><img className="salmon rounded-lg" src="./img/chicken.jpg" alt="" /></SwiperSlide>
-          <SwiperSlide><img className="spaghetti object-bottom rounded-lg" src="./img/spaghett.jpg" alt="" /></SwiperSlide>
-          <SwiperSlide><img className="telur rounded-lg" src="./img/telur.jpg" alt="" /></SwiperSlide>
-          <SwiperSlide><img className="mie rounded-lg" src="./img/mie.jpg" alt="" /></SwiperSlide>
-        </Swiper>
-        
-        <Footer></Footer>
-      </div>
-    </>
+return(
+  <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/About' element={<About></About>}></Route>
+      </Routes>
+    </Router>
+  </>
   )
 }
 
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,39 @@
+// import React from 'react'
+import { Navbar } from '../components/Navbar'
+import { Main_content_wrap } from '../components/Main_content_wrap'
+import { Footer } from '../components/Footer'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// import 'swiper/css';
+
+export const Home = () => {
+    return(
+        <>
+            <Navbar></Navbar>
+            <div className="nama">
+                <h2>SAKU.co</h2>
+                <h3>Sistem Pemesanan Kantin UPNVJ</h3>
+            </div>
+            
+            <Main_content_wrap></Main_content_wrap>
+
+            <Swiper
+                spaceBetween={10}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide><img className="sushi rounded-lg" src="./img/sushi.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className="burger rounded-lg" src="./img/burgir.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className="salmon rounded-lg" src="./img/chicken.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className="spaghetti object-bottom rounded-lg" src="./img/spaghett.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className="telur rounded-lg" src="./img/telur.jpg" alt="" /></SwiperSlide>
+                <SwiperSlide><img className="mie rounded-lg" src="./img/mie.jpg" alt="" /></SwiperSlide>
+            </Swiper>
+
+            <Footer></Footer>
+        </>
+    )
+}
+
+export default Home
